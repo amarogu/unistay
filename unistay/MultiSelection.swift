9cc2dbb1ea84f3fc3ca544fa17746af8acfd61ba
Gustavo <gumarinho090306@gmail.com>
2023-09-18 09:05:59 -0300

    Multi selection of publication imgs

---
+++ unistay/MultiSelection.swift
@@ -0,0 +1,22 @@
+//
+//  MultiSelection.swift
+//  unistay
+//
+//  Created by Gustavo Amaro on 17/09/23.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct MultiSelection: View {
+    @Binding var presented: Bool
+    @State var photosPickerItem: [PhotosPickerItem] = []
+    @State var array: [UIImage] = []
+    var body: some View {
+       Text("hi")
+        }
+    }
+
+
+
+
--- unistay/SignUpPublisherFifth.swift
+++ unistay/SignUpPublisherFifth.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct SignUpPublisherFifth: View {
     
@@ -21,11 +22,17 @@ struct SignUpPublisherFifth: View {
     var commit: ()->() = { }
     @FocusState private var isFocused: Bool
     
+    @State var croppedImage: UIImage?
+    @State var presented: Bool = false
+    
+    @State var show: Bool = false
+    @State private var photosPickerItem = [PhotosPickerItem]()
+    @State private var array = [UIImage]()
+    
     var body: some View {
         GeometryReader {
             geo in
             let width = geo.size.width
-            let height = geo.size.height
             NavigationStack {
                 ZStack {
                     Color("BackgroundColor").edgesIgnoringSafeArea(.all)
@@ -36,6 +43,62 @@ struct SignUpPublisherFifth: View {
                             Group {
                                 TextInputField(input: $yourLocation, placeholderText: "Your location", placeholderIcon: "location.circle")
                                 MenuField(items: visibility, menuSelection: $menuSelection, icon: menuSelection == "Visible" ? "eye" : "eye.slash", placeholder: styledText(type: "Regular", size: 13, content: menuSelection))
+                                Button(action: {
+                                    show.toggle()
+                                    array = []
+                                    Task {
+                                        for item in photosPickerItem {
+                                            if let imageData = try? await item.loadTransferable(type: Data.self), let image = UIImage(data: imageData) {
+                                                DispatchQueue.main.async {
+                                                    
+                                                        self.array.append(image)
+                                                    
+                                                }
+                                            }
+                                        }
+                                        
+                                    }
+                                }) {
+                                    if !array.isEmpty {
+                                        HStack() {
+                                            let _ = print("loaded")
+                                            ZStack {
+                                                Image(uiImage: array[0]).resizable().aspectRatio(contentMode: .fill).frame(maxWidth: 40, maxHeight: 40).scaleEffect(1.4).clipped().cornerRadius(5).padding(.trailing, 4)
+                                                if array.count > 1 {
+                                                    Image(uiImage: array[1] ).resizable().aspectRatio(contentMode: .fill).frame(maxWidth: 40, maxHeight: 40).scaleEffect(1.4).clipped().cornerRadius(5).padding(.trailing, 4).zIndex(-1).opacity(0.6).offset(x: 6, y: 3)
+                                                    if array.count > 2 {
+                                                        Image(uiImage: array[2] ).resizable().aspectRatio(contentMode: .fill).frame(maxWidth: 40, maxHeight: 40).scaleEffect(1.4).clipped().cornerRadius(5).padding(.trailing, 4).zIndex(-1).opacity(0.4).offset(x: 10, y: 5)
+                                                    }
+                                                }
+                                            }.padding(.trailing, 10)
+                                            VStack(alignment: .leading) {
+                                                styledText(type: "Regular", size: 12, content: "Update you publication images").foregroundColor(Color("BodyEmphasized"))
+                                                styledText(type: "Regular", size: 12, content: "Click here to change the images you have selected").foregroundColor(Color("Body")).opacity(0.8).multilineTextAlignment(.leading)
+                                            }
+                                            Spacer()
+                                        }.frame(maxWidth: .infinity).padding(.vertical, 10).padding(.horizontal, 20).background(Color("SearchBar")).cornerRadius(5).padding(.vertical, 1)
+                                    } else {
+                                        HStack {
+                                            Image(systemName: "camera").font(.system(size: 14))
+                                            styledText(type: "Regular", size: 13, content: "Upload publication images")
+                                            Spacer()
+                                        }.frame(maxWidth: .infinity).padding(.vertical, 10).padding(.horizontal, 20).background(Color("SearchBar")).cornerRadius(5).padding(.vertical, 1)
+                                    }
+                                }.photosPicker(isPresented: $show, selection: $photosPickerItem).onChange(of: photosPickerItem) { newValue in
+                                    array = []
+                                    Task {
+                                        for item in newValue {
+                                            if let imageData = try? await item.loadTransferable(type: Data.self), let image = UIImage(data: imageData) {
+                                                DispatchQueue.main.async {
+                                                    self.array.append(image)
+                                                }
+                                            }
+                                        }
+                                        
+                                    }
+                                    
+                                }
+                                
                             }
                             Button(action: {
                                 viewModel.signUp(inputs: [menuSelection, yourLocation], isToggled: $isToggled)
